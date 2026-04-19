b12527d8b595a05981ef1fe12b187eda95e3e257
BlackMambaJesus <negrokageTV@gmail.com>
2026-04-19 16:59:18 -0400

    Editing the house stuf

--- come-shell-or-high-water/Scripts/Global_Variables.gd
+++ come-shell-or-high-water/Scripts/Global_Variables.gd
@@ -2,3 +2,4 @@ extends Node
 
 var characters_in_scene = []
 var memories_acquired = 0
+var start_items_acquired = 0
---
+++ come-shell-or-high-water/Scripts/start_game.gd
@@ -0,0 +1,13 @@
+extends Area3D
+
+@onready var gpu_particles_3d: GPUParticles3D = $"../GPUParticles3D"
+
+func start_particles():
+	gpu_particles_3d.emitting = true
+
+func _on_area_entered(area: Area3D) -> void:
+	if GlobalVariables.start_items_acquired >= 2:
+		call_deferred("start_ending")
+
+func start_ending():
+	get_tree().change_scene_to_file("res://Scenes/map_sands_of_time.tscn")
