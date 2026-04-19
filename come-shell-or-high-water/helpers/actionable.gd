25531bcf8ffbef024707829a42779e3af60f1ced
lightstriderap-png <lightstriderap@gmail.com>
2026-04-19 13:08:22 -0400

    Minor Changes
    - Minor Stuff

--- come-shell-or-high-water/helpers/actionable.gd
+++ come-shell-or-high-water/helpers/actionable.gd
@@ -1,7 +1,8 @@
 extends Area3D
 
 @onready var crabeau: CharacterBody3D = $"../../Crabeau"
-
+var hasbeeninteracted = false
+@onready var object_area: Area3D = $"."
 
 
 @export var dialogue_resource: DialogueResource
@@ -10,5 +11,8 @@ extends Area3D
 
 func action() -> void:
 	crabeau.is_talking = true
+	hasbeeninteracted = true
+	set_deferred("monitoring", false)
+	set_deferred("monitorable", false)
 	print("Crabeau is yappin'")
 	DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start)
